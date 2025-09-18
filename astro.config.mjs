4a4e255c1bfb4b9ccf1146b2d5ca7e7965a5dbb3
Ash <ashzhenbi@gmail.com>
2025-09-19 08:17:12 +1000

    project start

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,10 @@ import { defineConfig } from 'astro/config';
 
 import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), sentry(), spotlightjs()],
+    integrations: [mdx(), sitemap(), sentry(), spotlightjs(), react()],
 });
